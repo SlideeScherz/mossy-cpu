2120e4364ff05f0b7e81be33f3b220735571ed79
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-03-21 19:36:34 -0400

    mocha-ts working good here

---
+++ tests/memory.test.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+
+describe("Memory", () => {
+
+  // the tests container
+  it("Memory Capacity", () => {
+
+    // the single test
+    //const CPU = new Cpu(500, "CPU Test", false); 
+
+    /* detect retina */
+    expect(false).to.be.false; // Do I need to explain anything? It's like writing in English!
+
+    /* fps limit */
+    expect(30).to.equal(30); // As I said 3 lines above
+
+    //replace with real CPU containers
+    var emptyArray = [];
+
+    expect(emptyArray).to.be.empty; // emitters property is an array and for this test must be empty, this syntax works with strings too
+    
+    /*
+    //test constructor
+    expect(CPU)
+      .to.be.an("object")
+      .to.have.property("value")
+      .to.equal(500); 
+    */
+  });
+});
---
+++ tests/test.ts
@@ -0,0 +1,31 @@
+import { Cpu } from "../src/hardware/Cpu";
+import { expect } from "chai";
+
+describe("CPU tests", () => {
+
+  // the tests container
+  it("checking default CPU", () => {
+
+    // the single test
+    //const CPU = new Cpu(500, "CPU Test", false); 
+
+    /* detect retina */
+    expect(false).to.be.false; // Do I need to explain anything? It's like writing in English!
+
+    /* fps limit */
+    expect(30).to.equal(30); // As I said 3 lines above
+
+    //replace with real CPU containers
+    var emptyArray = [];
+
+    expect(emptyArray).to.be.empty; // emitters property is an array and for this test must be empty, this syntax works with strings too
+    
+    /*
+    //test constructor
+    expect(CPU)
+      .to.be.an("object")
+      .to.have.property("value")
+      .to.equal(500); 
+    */
+  });
+});
